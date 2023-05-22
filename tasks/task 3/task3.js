ff0ce7d0dfbf15003f18f557609f85f933d4de88
Youssouf SISSOKOmrsissoko@Sissokos-MacBook-Pro ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-05-22 17:18:30 +0000

    v.1

---
+++ tasks/task 3/task3.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+fs.writeFile('welcome.txt', 'Hello Node', function(err) {
+    if (err) {
+       return console.error(err);
+    }
+    console.log("Data written successfully!");
+});
+
+
+fs.readFile('welcome.txt', function (err, data) {
+    if (err) {
+       return console.error(err);
+    }
+    console.log(`Data of Hello.txt: ${data}`);
+ });
